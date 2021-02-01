3e6e98f2fb7c32d525b72ebd97042eb167d485d3
Derek Smithson <derek.smithson@knightware.net>
2021-01-31 21:50:06 -0700

    Updates over the weekend

---
+++ src/Vista.Controllers.ScreenMaster3.RabbitMQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vista.Controllers.ScreenMaster3.RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
